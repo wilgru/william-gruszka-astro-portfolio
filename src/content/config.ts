432aae6156fb02a84e681192f59dc58e8801e276
William Gruszka <william.gruszka@gmail.com>
2024-09-12 20:17:49 +1000

    added initial overall layout

---
+++ src/content/config.ts
@@ -0,0 +1,27 @@
+// 1. Import utilities from `astro:content`
+import { z, reference, defineCollection } from "astro:content";
+
+// 2. Define your collection(s)
+const tagCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
+    colour: z.string(),
+  }),
+});
+
+const projectCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(reference("tag")),
+    // image: z.string().optional(),
+  }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  tag: tagCollection,
+  projects: projectCollection,
+};
---
+++ src/content/projects/itunalyzer.md
@@ -0,0 +1,9 @@
+---
+title: "Itunalyzer"
+# author: ben-holmes # references `src/content/authors/ben-holmes.json`
+tags:
+  - python # references `src/content/blog/python.json`
+  # - my-year-in-review # references `src/content/blog/my-year-in-review.md`
+---
+
+Your blog post content here.
