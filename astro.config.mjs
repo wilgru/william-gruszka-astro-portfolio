34a062c3ce04b8ab44298d92a57c57cf9ffbebcf
williamgruszka <william.gruszka@gmail.com>
2025-03-03 20:04:13 +1100

    update site URL in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wilgru.github.io',
+  site: 'https://wilgru.github.io/william-gruszka-astro-portfolio/',
 });
